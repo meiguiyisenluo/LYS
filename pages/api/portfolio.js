e3a1871ed3e57a6554c403bf7b3e1d2d9ca14676
lys <1402175410@qq.com>
2025-01-24 16:17:57 +0800

    fix

--- pages/api/portfolio.js
+++ pages/api/portfolio.js
@@ -3,18 +3,12 @@ import { join } from "path";
 
 export default function handler(req, res) {
   const portfolioData = join(process.cwd(), "/data/portfolio.json");
-  if (process.env.NODE_ENV === "development") {
-    if (req.method === "POST") {
-      fs.writeFileSync(
-        portfolioData,
-        JSON.stringify(req.body),
-        "utf-8",
-        (err) => console.log(err)
-      );
-    } else {
-      res
-        .status(200)
-        .json({ name: "This route works in development mode only" });
-    }
+  if (req.method === "POST") {
+    fs.writeFileSync(
+      portfolioData,
+      JSON.stringify(req.body),
+      "utf-8",
+      (err) => console.log(err)
+    );
   }
 }
